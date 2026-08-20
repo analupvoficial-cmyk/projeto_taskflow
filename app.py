897c6f8292f5b032bf4bf93285bd6705c97cefd1
senai_26_27 <analupv.oficial@gmail.com>
2026-08-20 14:25:10 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+# importe de biblioteca (instalar no terminal)
+from flask import Flask, render_template, request
+
+# criar objeto flask "apelido - app"
+app = Flask(__name__)
+
+# base de dados fake
+base_fake = []
+# rotas
+# toda rora é acompanhada de uma função (def)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/atividades/criar', methods=['GET','POST'])
+def criar_atividade():
+    if request.method == 'POST':
+        # aqui recebe dados do formulário
+        nome_atividade = request.form.get('form_nome')
+        data_atividade = request.form.get('form_data')
+        tipo_atividade = request.form.getlist('form_tipo')
+
+        dados = {
+            'nome': nome_atividade,
+            'data': data_atividade,
+            'tipo': tipo_atividade
+        }
+        print(f'dados: {dados}')
+        base_fake.append(dados)
+        print(f'base_fake: {base_fake}')
+        return render_template('criar_atividade.html',dados_atividade=base_fake)
+
+    return render_template('criar_atividade.html')
+
+@app.route('/atividades_listar')
+def listar_atividades():
+    return render_template('listar_atividades.html', dados_atividade=base_fake)
+
+@app.route('/pessoa')
+def pessoa():
+    return render_template('pessoa.html')
+
+
+
+# iniciar aplicação web
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
+# nada deve ser colocado abaixo
